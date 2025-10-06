b02c090922d4ea1850331f6495cbc592af08e20c
lahirulakshan129 <lahirulakshan129@gmail.com>
2025-10-06 21:31:30 +0530

    Update navigation links to use absolute paths

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -11,12 +11,12 @@ const HomePage: React.FC = () => {
       <nav className="navbar">
         <div className="logo">⟨⟩ Lahiru L</div>
         <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About me</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#resume">Resume</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/aboutme">About me</a></li>
+          <li><a href="/skills">Skills</a></li>
+          <li><a href="/projects">Projects</a></li>
+          <li><a href="/resume">Resume</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
       </nav>
 
